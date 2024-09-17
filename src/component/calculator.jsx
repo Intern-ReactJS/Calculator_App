9f7ac1da50453d0ab683a3bb037972a74306e3bd
Huy Huy <muonnvtabth03@gmail.com>
2024-09-17 23:25:31 +0700

    update file Button.jsx Calculator

--- src/component/calculator.jsx
+++ src/component/calculator.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./calculator.scss";
+import Button from "./Button";
 
 function Calculator() {
   const [input, setInput] = useState("");
@@ -28,22 +29,22 @@ function Calculator() {
         <h2>{result}</h2>
       </div>
       <div className="buttons">
-        <button onClick={() => handleClick("7")}>7</button>
-        <button onClick={() => handleClick("8")}>8</button>
-        <button onClick={() => handleClick("9")}>9</button>
-        <button onClick={() => handleClick("+")}>+</button>
-        <button onClick={() => handleClick("4")}>4</button>
-        <button onClick={() => handleClick("5")}>5</button>
-        <button onClick={() => handleClick("6")}>6</button>
-        <button onClick={() => handleClick("-")}>-</button>
-        <button onClick={() => handleClick("1")}>1</button>
-        <button onClick={() => handleClick("2")}>2</button>
-        <button onClick={() => handleClick("3")}>3</button>
-        <button onClick={() => handleClick("*")}>*</button>
-        <button onClick={() => handleClick("0")}>0</button>
-        <button onClick={() => handleClick("/")}>/</button>
-        <button onClick={() => handleClick("=")}>=</button>
-        <button onClick={() => handleClick("C")}>C</button>
+        <Button value="7" onClick={handleClick} />
+        <Button value="8" onClick={handleClick} />
+        <Button value="9" onClick={handleClick} />
+        <Button value="+" onClick={handleClick} />
+        <Button value="4" onClick={handleClick} />
+        <Button value="5" onClick={handleClick} />
+        <Button value="6" onClick={handleClick} />
+        <Button value="-" onClick={handleClick} />
+        <Button value="1" onClick={handleClick} />
+        <Button value="2" onClick={handleClick} />
+        <Button value="3" onClick={handleClick} />
+        <Button value="*" onClick={handleClick} />
+        <Button value="0" onClick={handleClick} />
+        <Button value="/" onClick={handleClick} />
+        <Button value="=" onClick={handleClick} />
+        <Button value="C" onClick={handleClick} />
       </div>
     </div>
   );
